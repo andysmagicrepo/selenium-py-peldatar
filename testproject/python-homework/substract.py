64a74f29efb08cdfeadbe10b8daac62db1762494
Bazsalya András <abazsalya@gmail.com>
2021-06-20 20:42:33 +0200

    Python feladat - Python Adatok OK

---
+++ testproject/python-homework/substract.py
@@ -0,0 +1,3 @@
+a = int(input("Kérem az első számjagyet (a=kissebbítendő)"))
+b = int(input("Kérem a második számjegyet (b=kivonandó)"))
+print("a (kissebbítendő=",a,")" " - b (kivonandó)=",b,") = c (Eredmény a=",a,"- b=",b,") =",a-b,)
