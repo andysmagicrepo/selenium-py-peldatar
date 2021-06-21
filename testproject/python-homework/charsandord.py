ed2dec622f50bd9f4e5d8169e9ca081300ac7e51
Bazsalya András <abazsalya@gmail.com>
2021-06-21 22:29:38 +0200

    Feladat: Python for ciklus gyakorlása (már csak z-ig ír ki)

--- testproject/python-homework/charsandord.py
+++ testproject/python-homework/charsandord.py
@@ -1,5 +1,6 @@
 x = 97
-for i in range(9):
+for i in range(8):
     print(chr(x), x, chr(x+1), (x+1), chr(x+2), (x+2))
 #   print(chr(x), hex(x), chr(x + 1), hex(x + 1), chr(x + 2), hex(x + 2))
     x += 3
+print(chr(x), x, chr(x + 1), (x + 1))
